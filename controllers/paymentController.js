a497061150f57152d23c4c614aed9deb73bdedf5
Karan <Karandp505@gmail.com>
2025-11-03 11:46:11 +0530

    work on email

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -13,11 +13,6 @@ const { updateProductPageGetdonation } = require('./productPageController');
 
 
 
-const SibApiV3Sdk = require("@sendinblue/client");
-
-const brevo = new SibApiV3Sdk.TransactionalEmailsApi();
-brevo.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
-
  
 
 
